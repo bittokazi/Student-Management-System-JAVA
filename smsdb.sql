3ef9d373ac7b688704857b10c55b7d1afc716ea8
Bitto Kazi <bitto.kazi@gmail.com>
2017-01-24 21:55:14 +0600

    database

---
+++ smsdb.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 24, 2017 at 03:54 PM
+-- Server version: 5.5.8
+-- PHP Version: 5.3.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `smsdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_info`
+--
+
+CREATE TABLE IF NOT EXISTS `student_info` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `dob` text NOT NULL,
+  `fname` text NOT NULL,
+  `mname` text NOT NULL,
+  `sex` text NOT NULL,
+  `nationality` text NOT NULL,
+  `religion` text NOT NULL,
+  `blood` text NOT NULL,
+  `subject` text NOT NULL,
+  `admission` text NOT NULL,
+  `address` text NOT NULL,
+  `studentid` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `student_info`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `un` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `un`) VALUES
+(1, 'kazi'),
+(3, 'bitto');
